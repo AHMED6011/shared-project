e26d985a43e5dfeda462e994a1d0421cc9fdf7be
AHMED6011 <ahmedallawi010@gmail.com>
2025-01-17 14:58:41 +0100

    added html and js files to the project

---
+++ main.js
@@ -0,0 +1,9 @@
+const titleH1 = document.querySelector("h1");
+titleH1.style.color = "red";
+titleH1.style.fontSize = "50px";
+titleH1.style.textAlign = "center";
+titleH1.style.fontFamily = "Arial";
+titleH1.style.backgroundColor = "yellow";
+titleH1.style.padding = "20px";
+titleH1.style.border = "2px solid black";
+console.log(`this text we got it from html file ${titleH1}`);
